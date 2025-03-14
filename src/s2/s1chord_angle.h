07798ceba189fc7b11a144d1411008626b4eae9d
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2025-03-14 15:35:57 +0100

    More always_inline things.

--- src/s2/s1chord_angle.h
+++ src/s2/s1chord_angle.h
@@ -216,7 +216,7 @@ class S1ChordAngle {
   // Comparison operators.
   friend bool operator==(S1ChordAngle x, S1ChordAngle y);
   friend bool operator!=(S1ChordAngle x, S1ChordAngle y);
-  friend bool operator<(S1ChordAngle x, S1ChordAngle y);
+  [[gnu::always_inline]] [[clang::always_inline]] friend bool operator<(S1ChordAngle x, S1ChordAngle y);
   friend bool operator>(S1ChordAngle x, S1ChordAngle y);
   friend bool operator<=(S1ChordAngle x, S1ChordAngle y);
   friend bool operator>=(S1ChordAngle x, S1ChordAngle y);
@@ -430,7 +430,7 @@ inline bool operator!=(S1ChordAngle x, S1ChordAngle y) {
   return x.length2() != y.length2();
 }
 
-inline bool operator<(S1ChordAngle x, S1ChordAngle y) {
+[[gnu::always_inline]] [[clang::always_inline]] inline bool operator<(S1ChordAngle x, S1ChordAngle y) {
   return x.length2() < y.length2();
 }
 
